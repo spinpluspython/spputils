549ef01100bdacca4e7f7874a6f05a6c38789ad5
Steinn Ymir Agustsson <sagustss@uni-mainz.de>
2018-04-20 14:40:17 +0200

    gaussian fit

--- Math/function.py
+++ Math/function.py
@@ -19,6 +19,11 @@ def gaussian(x, mu, sig):
     """
     return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
+def lorentz(p,x):
+    return p[2] / (1.0 + (x / p[0] - 1.0)**4 * p[1]**2)
+
+def errorfunc(p, x, z):
+    return lorentz(p, x) - z
 
 if __name__ == '__main__':
     main()
